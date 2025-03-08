75d77a64303132229026bfe1f61c74c25cb489bc
michal varyš <info@michalvarys.eu>
2025-03-08 14:36:59 +0100

    uat-tiptap-editor plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,15 @@
-import fs from "fs";
-import path from "path";
-
-//       : fs.existsSync(
-//           path.join(process.cwd(), "src", "plugins", "uat-tiptap-editor")
-//         )
-//       ? "./src/plugins/uat-tiptap-editor"
 export default ({ env }) => ({
-  "tiptap-editor": {
-    enabled: false,
-    resolve:
-      env("NODE_ENV", "development") === "production"
-        ? "/opt/node_modules/@ssupat/tiptap-editor"
-        : "node_modules/@ssupat/tiptap-editor",
-  },
   // "tiptap-editor": {
   //   enabled: true,
-  //   resolve: "./src/plugins/uat-tiptap-editor",
+  //   resolve:
+  //     env("NODE_ENV", "development") === "production"
+  //       ? "/opt/node_modules/@ssupat/tiptap-editor"
+  //       : "node_modules/@ssupat/tiptap-editor",
   // },
+  "tiptap-editor": {
+    enabled: true,
+    resolve: "./src/plugins/uat-tiptap-editor",
+  },
   upload: {
     config: {
       sizeLimit: 1000 * 1024 * 1024, // 256mb in bytes
